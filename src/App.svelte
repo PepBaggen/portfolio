cbdf7b58a6a1ef692a5c3d9def964aa92acba71e
PepBaggen <pepijnbaggen@gmail.com>
2023-10-08 00:45:12 +0200

    Your commit message here

---
+++ src/App.svelte
@@ -0,0 +1,212 @@
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHD-Danielle</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>    
+
+<style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Playfair+Display:wght@500&display=swap');
+
+ 
+        * {
+    margin: 0;
+    padding: 0;
+}
+        
+
+        body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: transparent;
+    background-image: url('https://drive.google.com/uc?id=1kdGGMsLsBFGMBiL4IyTmCK8v3UPUC94E');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    color: #f5f5f5; /* Creme white text color */
+    font-family: 'Montserrat', sans-serif;
+}
+
+
+
+
+
+
+
+
+
+        /* Remove white border around the background */
+
+
+        .all {
+            text-align: center;
+            font-size: 4vw;
+            padding-top: 7vh;
+        }
+
+        .all h2 {
+            font-size: 5vw; /* Adjust the font size as needed */
+        }
+
+        .all p {
+            padding-top: 3vh;
+            font-size: 4vw; /* Adjust the font size as needed */
+            /* Style the text with a gold color */
+            color: gold;
+            /* Add a subtle text-shadow for the glow effect */
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+
+        .all h3 {
+            font-size: 2vw; /* Adjust the font size as needed */
+            padding-top: 3vh;
+
+        }
+        .all h4{
+            font-size: 1.5vw; /* Adjust the font size as needed */
+            padding-top: 3vh;
+            padding: 13vw 13vw;
+
+
+        }
+         .section span{
+            font-size: 4vw;
+         }
+
+    
+        .contact-icons {
+            display: flex;
+            font-size: 1vw; /* Adjust the font size as needed */
+            justify-content: center;
+            padding-top: 2vh; /* Vertical centering (adjust value as needed) */
+
+        }
+
+        .contact-icons a {
+            display: inline-block;
+            margin: 0 20px;
+            color: white;
+            transition: color 0.5s ease;
+        }
+
+        .contact-icons a:hover {
+            color: #66D9EF; /* Change color on hover (you can adjust the color) */
+        }
+
+
+        .kaart {
+
+    text-align: center; /* Horizontal centering */
+    margin: 0 auto; /* Horizontal centering */
+
+}
+
+
+@media screen and (min-width: 400px) {
+            .all h2 {
+                font-size: 4vw; /* Adjust the font size as needed */
+            }
+
+            .all p {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            
+
+            .contact-icons {
+                font-size: 4vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+                font-size: 2vw;
+            }
+
+        }
+
+
+
+        @media screen and (min-width: 600px) {
+            .all h2 {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            .all p {
+                font-size: 2vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+                font-size: 2vw;
+            }
+
+            
+
+            .contact-icons {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+        }
+
+        @media screen and (min-width: 1200px) {
+            .all h2 {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            .section span{
+            font-size: 2vw;
+         }
+
+            .all p {
+                font-size: 1.5vw; /* Adjust the font size as needed */
+            }
+
+            .contact-icons {
+                font-size: 2vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+            font-size: 1vw; /* Adjust the font size as needed */
+            padding-top: 3vh;
+
+
+        }
+
+
+        }
+</style>
+
+
+
+
+<html lang="en">
+<body>
+    
+    <div class="all">
+        <div class="section" id="home">
+            <h2>Roaring PHD Party</h2>
+            <h4>Op vrijdag 10 November geef ik mijn phd party in Tutto Pepe in Baarn. Graag zou ik jou daar bij willen hebben. Als je in de gelegenheid bent en bereid bent om bij te dragen aan mijn feest, zou ik dat zeer waarderen. Elke bijdrage, groot of klein, helpt me om deze dag onvergetelijk te maken.
+            <h5> Als je zou willen doneren, kun je dat doen via het icoontje hieronder. Hartelijk dank! </h5>
+            <a href="https://betaalverzoek.rabobank.nl/betaalverzoek/?id=o3ND4NwXQK-dRM00CzwN2g">
+            <span>&#128181;</span></a>
+
+            <p>10 November - 20.30</p>
+            <p>Tutto Pepe, Amsterdamsestraatweg 42, Baarn</p>
+            <h3>Dresscode: Natural Gold</h3>
+        </div>
+        <div class="kaart">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2444.360101704942!2d5.260041177282808!3d52.21867515849431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66b5f7f4d74ad%3A0x967c09204d113cd5!2sTutto%20Pepe!5e0!3m2!1snl!2snl!4v1696714623038!5m2!1snl!2snl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="section" id="contact">
+            <div class="contact-icons">
+                <a href="mailto:davr@me.com" target="_blank">
+                    <i class="fas fa-envelope"></i>
+                </a>
+                <a href="0641557651" target="_blank">
+                    <i class="fas fa-phone"></i>
+                    <p>+31622141471</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
