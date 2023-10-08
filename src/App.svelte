80a2e4b459009d88dda94a7c1ef1f4b443f68fc0
PepBaggen <pepijnbaggen@gmail.com>
2023-10-08 23:29:15 +0200

    Add existing project files to Git

--- src/App.svelte
+++ src/App.svelte
@@ -22,7 +22,7 @@
     height: 100vh;
     width: 100vw;
     background-color: transparent;
-    background-image: url('https://drive.google.com/uc?id=1kdGGMsLsBFGMBiL4IyTmCK8v3UPUC94E');
+    background-image: url('https://drive.google.com/uc?id=1nGkdxMEhIPo0XrFn1gMBivmcyq68vD2e');
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
@@ -32,50 +32,56 @@
 
 
 
-
-
-
-
-
-
-        /* Remove white border around the background */
-
-
         .all {
-            text-align: center;
-            font-size: 4vw;
-            padding-top: 7vh;
-        }
+    text-align: center;
+    font-size: 4vw;
+    padding-top: 3vh; /* Adjust the padding-top value */
+    padding: 5vw 5vw;
+}
+
+.kaart {
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 2vh; /* Adjust the padding-top value */
+}
+
+.all h4 {
+    font-size: 1.5vw;
+    padding-top: 1vh; /* Adjust the padding-top value */
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    font-weight: normal; /* Remove bold formatting */
+}
+
+
+
 
         .all h2 {
             font-size: 5vw; /* Adjust the font size as needed */
         }
 
         .all p {
-            padding-top: 3vh;
+            padding-top: 10vh;
             font-size: 4vw; /* Adjust the font size as needed */
             /* Style the text with a gold color */
-            color: gold;
+            color: white;
             /* Add a subtle text-shadow for the glow effect */
             text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
         }
 
-        .all h3 {
-            font-size: 2vw; /* Adjust the font size as needed */
-            padding-top: 3vh;
-
-        }
-        .all h4{
-            font-size: 1.5vw; /* Adjust the font size as needed */
-            padding-top: 3vh;
-            padding: 13vw 13vw;
-
-
-        }
-         .section span{
+       
+     
+       
+         .section span a{
             font-size: 4vw;
-         }
+            padding-bottom: 15vh;
+            transition: color 0.5s ease;
+           
 
+         }
+         .section a:hover{
+            color: #66D9EF;
+
+         }
     
         .contact-icons {
             display: flex;
@@ -96,18 +102,49 @@
             color: #66D9EF; /* Change color on hover (you can adjust the color) */
         }
 
+.top {
+    text-align: center;
+    font-size: 1vw;
+    position: sticky;
+    
+}
+.top a{
+    color: white;
+    text-decoration: none;
+    transition: color 0.5s ease;
 
-        .kaart {
 
-    text-align: center; /* Horizontal centering */
-    margin: 0 auto; /* Horizontal centering */
+} 
+.top a:hover{
+    color: #66D9EF;
+}       
 
+.payment-icon {
+    display: inline-block;
+}
+
+.coin {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background-color: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Optional: Add a subtle shadow effect */
+    transition: background-color 0.3s ease;
+    margin-top: 15px;
+}
+
+.coin:hover {
+    background-color: #66D9EF; /* Change to blue on hover */
 }
 
 
-@media screen and (min-width: 400px) {
+
+
+
+@media screen and (min-width: 300px) {
             .all h2 {
                 font-size: 4vw; /* Adjust the font size as needed */
+                padding-top: 5vh;
             }
 
             .all p {
@@ -120,7 +157,42 @@
                 font-size: 4vw; /* Adjust the font size as needed */
             }
             .all h4{
-                font-size: 2vw;
+                padding-top: 5vh;
+                font-size: 2.5vw;
+            }
+            .top{
+                font-size: 1.5vw;
+            }
+
+        }
+
+
+
+
+
+
+
+@media screen and (min-width: 400px) {
+            .all h2 {
+                font-size: 4vw; /* Adjust the font size as needed */
+                padding-top: 5vh;
+            }
+
+            .all p {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            
+
+            .contact-icons {
+                font-size: 4vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+                padding-top: 5vh;
+                font-size: 2.5vw;
+            }
+            .top{
+                font-size: 1.5vw;
             }
 
         }
@@ -136,13 +208,17 @@
                 font-size: 2vw; /* Adjust the font size as needed */
             }
             .all h4{
+                padding-top: 5vh;
                 font-size: 2vw;
             }
 
             
 
             .contact-icons {
-                font-size: 3vw; /* Adjust the font size as needed */
+                font-size: 2vw; /* Adjust the font size as needed */
+            }
+            .top{
+                font-size: 1.5vw;
             }
         }
 
@@ -160,7 +236,7 @@
             }
 
             .contact-icons {
-                font-size: 2vw; /* Adjust the font size as needed */
+                font-size: 1.6vw; /* Adjust the font size as needed */
             }
             .all h4{
             font-size: 1vw; /* Adjust the font size as needed */
@@ -168,6 +244,9 @@
 
 
         }
+        .top{
+                font-size: 1vw;
+            }
 
 
         }
@@ -180,28 +259,35 @@
 <body>
     
     <div class="all">
+        <div class="top"><a href="https://docs.google.com/viewer?url=https://drive.google.com/uc?id=1ycdMVBYuo4p8IVZi078EtwgAuSe4wJtH&embedded=true" target="_blank"><h1>Thesis Link</h1></a></div>
         <div class="section" id="home">
-            <h2>Roaring PHD Party</h2>
-            <h4>Op vrijdag 10 November geef ik mijn phd party in Tutto Pepe in Baarn. Graag zou ik jou daar bij willen hebben. Als je in de gelegenheid bent en bereid bent om bij te dragen aan mijn feest, zou ik dat zeer waarderen. Elke bijdrage, groot of klein, helpt me om deze dag onvergetelijk te maken.
-            <h5> Als je zou willen doneren, kun je dat doen via het icoontje hieronder. Hartelijk dank! </h5>
-            <a href="https://betaalverzoek.rabobank.nl/betaalverzoek/?id=o3ND4NwXQK-dRM00CzwN2g">
-            <span>&#128181;</span></a>
+            <h2>PhD ceremonie Daniëlle</h2>
+            <h4>Op dinsdag 31 oktober om 16:15 zal Daniëlle haar proefschrift verdedigen aan de Universiteit Leiden. Op deze pagina vindt je tevens ook haar thesis. 
+            <br><br> Als je haar een cadeau wilt geven, zij wilt graag een antieke stationsbank voor in de tuin. Een kleine bijdrage is daarvoor heel welkom. Een bijdrage kan met het ronde icoontje hieronder.
+            <br>Namens Pepijn en Mieke (paranimfen)
+        </h4>
+            <a href="https://tikkie.me/pay/e684qvqcko6vili6aoep" target="_blank">
+                <div class="payment-icon">
+                    <a href="https://tikkie.me/pay/e684qvqcko6vili6aoep" target="_blank">
+                        <div class="coin"></div>
+                    </a>
+                </div>   </a>
 
-            <p>10 November - 20.30</p>
-            <p>Tutto Pepe, Amsterdamsestraatweg 42, Baarn</p>
-            <h3>Dresscode: Natural Gold</h3>
+            <p>31 oktober 2023
+                <br><br>Academie Gebouw
+            <br>Rapenburg 73, 2311 GJ Leiden</p>
+            
         </div>
         <div class="kaart">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2444.360101704942!2d5.260041177282808!3d52.21867515849431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66b5f7f4d74ad%3A0x967c09204d113cd5!2sTutto%20Pepe!5e0!3m2!1snl!2snl!4v1696714623038!5m2!1snl!2snl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d156656.71680113432!2d4.196923686718741!3d52.15685240000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c6f2492d2c39%3A0xa86e57c2a94815d9!2sAcademiegebouw%20Leiden!5e0!3m2!1snl!2snl!4v1696793580081!5m2!1snl!2snl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        </div>
         <div class="section" id="contact">
             <div class="contact-icons">
                 <a href="mailto:davr@me.com" target="_blank">
                     <i class="fas fa-envelope"></i>
                 </a>
-                <a href="0641557651" target="_blank">
+                <a href="" target="_blank">
                     <i class="fas fa-phone"></i>
-                    <p>+31622141471</p>
+                    +31622141471
                 </a>
             </div>
         </div>
